1c411b8d85adef7b8f3f11289fecce50b70241a5
Raj Rajhans <rajrajhans@Rajs-MacBook-Pro.local>
2023-10-30 23:24:40 +0530

    feat: script for installing common softwares

---
+++ install/common_software.sh
@@ -0,0 +1,42 @@
+brew_install() {
+    echo "\n⚙️⚙️ Installing $1"
+    if brew list $1 &>/dev/null; then
+        echo "✅ ${1} is already installed"
+    else
+        brew install $2 $1 && echo "✅ $1 is installed"
+    fi
+}
+
+packages=(
+  git
+  lazygit
+)
+
+for package in "${packages[@]}"; do
+  brew_install "$package"
+done
+
+casks=(
+  # browsers
+  google-chrome
+  firefox
+
+  # dev tools
+  iterm2
+  visual-studio-code
+  sublime-text
+  docker
+  dbeaver-community
+  insomnia
+
+  # utility tools
+  vlc
+  bitwarden
+  rectangle
+  stats
+  cloudflare-warp
+)
+
+for cask in "${casks[@]}"; do
+  brew_install "$cask" "--cask"
+done
---
+++ install/nix.sh
@@ -0,0 +1,2 @@
+# install nix
+curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
