1f8c03e779a6c707e9253b06c8cf05cdd440632d
Raj Rajhans <me@rajrajhans.com>
2026-02-12 13:42:34 +0530

    fix: escape '' in nix multiline string for yazi wrapper

--- nixpkgs/home-manager/server.nix
+++ nixpkgs/home-manager/server.nix
@@ -161,7 +161,7 @@ in
       function y() {
         local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
         command yazi "$@" --cwd-file="$tmp"
-        IFS= read -r -d '' cwd < "$tmp"
+        IFS= read -r -d ''' cwd < "$tmp"
         [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
         rm -f -- "$tmp"
       }
